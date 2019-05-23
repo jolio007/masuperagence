9ea7e1e9841678b3add829894caeb858faf34744
Joel Bushiri <joel.bushiri@aetlis.com>
2019-05-23 16:51:16 +0200

    minute 23:23 3/16

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,14 +4,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 
 class PropertyController extends AbstractController
 {
+    /**
+     * @var PropertyRepository 
+     */
+
+    private $repository;
+
+    public function __construct(PropertyRepository $repository){
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index(): Response{
+
+        $property = $this->repository->findAll();
+        dump($property);
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
         ]);
